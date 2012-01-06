4feac6916f0e2c9d9fea15dfce88dc57db4fe9df
nrporter <nrporter1@gmail.com>
2012-01-06 10:59:34 -0500

    Installing the Twitter Tweet blocktype (version 2011041900).

---
+++ htdocs/blocktype/twittertweet/lang/de.utf8/blocktype.twittertweet.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage blocktype-twittertweet
+ * @author     Gregor Anzelj
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2011 Gregor Anzelj, gregor.anzelj@gmail.com
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'Twittern-Button';
+$string['description'] = 'Fügt den Twitter Twittern-Button hinzu.';
+
+$string['showtitle'] = 'Blocktitel anzeigen?';
+
+$string['datacount'] = 'Layout Stil';
+$string['datacountdesc'] = 'Legt die  Anzeige des Twittern-Buttons fest.';
+$string['datacountnone'] = 'Ohne Zähler';
+$string['datacounthorizontal'] = 'Horizontale Zähler';
+$string['datacountvertical'] = 'Vertikale Zähler';
+
+$string['align'] = 'Ausrichtung';
+$string['alignleft'] = 'Links';
+$string['aligncenter'] = 'Zentriert';
+$string['alignright'] = 'Rechts';
+?>
---
+++ htdocs/blocktype/twittertweet/lang/en.utf8/blocktype.twittertweet.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage blocktype-twittertweet
+ * @author     Gregor Anzelj
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2011 Gregor Anzelj, gregor.anzelj@gmail.com
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'Twitter Tweet';
+$string['description'] = 'Add Twitter Tweet button';
+
+$string['showtitle'] = 'Show Block Title?';
+
+$string['datacount'] = 'Layout Style';
+$string['datacountdesc'] = 'Determine the size and amount of social context next to the button.';
+$string['datacountnone'] = 'No count';
+$string['datacounthorizontal'] = 'Horizontal count';
+$string['datacountvertical'] = 'Vertical count';
+
+$string['tweettext'] = 'Tweet text';
+$string['tweettextdesc'] = 'This is the text that people will include in their Tweet when they share from your website.';
+
+$string['align'] = 'Align';
+$string['alignleft'] = 'Left';
+$string['aligncenter'] = 'Center';
+$string['alignright'] = 'Right';
+?>
